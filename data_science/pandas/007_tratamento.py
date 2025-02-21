c1e6986b9db77f93c5a179c59bec5efbfeb9af93
AdrianoCorreia94 <adriano.correia94@hotmail.com>
2025-02-21 18:19:22 -0300

    novo modo de verificar nulos

--- data_science/pandas/007_tratamento.py
+++ data_science/pandas/007_tratamento.py
@@ -3,15 +3,16 @@
 import pandas as pd 
 
 df = pd.read_csv('players.csv')
-
+'''
 t1 = df.fillna(0)  # troca os valores Nan por 0
 t2 = df.fillna('vazio')  # também é possivel trocar por string
 
 print(df)   # df original
 print(t1)   # df com troca de NaN por 0
 print(t2)   # df com troca de NaN pela string 'vazio'
+'''
+
 
 # visualizar os valores Nan 
-teste = df.isna()   # retorna true or false para verificar se é null
-
-print(teste)
+print(df.isnull())   # retorna true or false para verificar se é null
+print(df.isnull().values.any())
